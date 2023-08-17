4c950c494d2dd874e161c33810b9203a60db786a
Lucas-Masaba <masabaluke@gmail.com>
2023-08-17 23:33:10 +0300

    Index Routes

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ import EditContact, {
   action as editAction,
 } from "./routes/edit";
 import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
 import "./index.css";
 
 const router = createBrowserRouter([
@@ -23,6 +24,9 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
+      { 
+        index: true, element: <Index /> 
+      },
       {
         path: "contacts/:contactId",
         element: <Contact />,
