d960ac851dd3d0cbb0f476d85b0c3d75aa19f8b2
Lucas-Masaba <masabaluke@gmail.com>
2023-08-21 22:20:39 +0300

    Configure search

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,5 +1,6 @@
-import { Outlet, NavLink, useLoaderData, Form, redirect, useNavigation } from "react-router-dom";
+import { Outlet, NavLink, useLoaderData, Form, redirect, useNavigation, useSubmit } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
+import { useEffect } from "react";
 
 export async function action() {
   const contact = await createContact();
@@ -7,27 +8,38 @@ export async function action() {
 }
 
 
-export async function loader() {
-  const contacts = await getContacts();
-  return { contacts };
+export async function loader({ request }) {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
+  return { contacts, q };
 }
 
 export default function Root() {
-  const { contacts } = useLoaderData();
+  const { contacts, q } = useLoaderData();
   const navigation = useNavigation();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    document.getElementById("q").value = q;
+  }, [q]);
 
   return (
     <>
       <div id="sidebar">
         <h1>React Router Contacts</h1>
         <div>
-          <form id="search-form" role="search">
+          <Form id="search-form" role="search">
             <input
               id="q"
               aria-label="Search contacts"
               placeholder="Search"
               type="search"
               name="q"
+              defaultValue={q}
+              onChange={(event) => {
+                submit(event.currentTarget.form);
+              }}
             />
             <div
               id="search-spinner"
@@ -38,7 +50,7 @@ export default function Root() {
               className="sr-only"
               aria-live="polite"
             ></div>
-          </form>
+          </Form>
           <Form method="post">
             <button type="submit">New</button>
           </Form>
@@ -48,13 +60,13 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <NavLink                     to={`contacts/${contact.id}`}
+                  <NavLink to={`contacts/${contact.id}`}
                     className={({ isActive, isPending }) =>
                       isActive
                         ? "active"
                         : isPending
-                        ? "pending"
-                        : ""
+                          ? "pending"
+                          : ""
                     }>
                     {contact.first || contact.last ? (
                       <>
@@ -75,9 +87,9 @@ export default function Root() {
           )}
         </nav>
       </div>
-      <div id="detail"         className={
-          navigation.state === "loading" ? "loading" : ""
-        }><Outlet /></div>
+      <div id="detail" className={
+        navigation.state === "loading" ? "loading" : ""
+      }><Outlet /></div>
     </>
   );
 }
